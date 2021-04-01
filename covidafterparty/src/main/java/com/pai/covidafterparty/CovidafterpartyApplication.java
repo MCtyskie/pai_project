68f7d6b4074c06497d6b3f461a317ffe5a0716b1
MCtyskie <micsul000@utp.edu.pl>
2021-04-01 13:20:39 +0200

    project created

---
+++ covidafterparty/src/main/java/com/pai/covidafterparty/CovidafterpartyApplication.java
@@ -0,0 +1,13 @@
+package com.pai.covidafterparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidafterpartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidafterpartyApplication.class, args);
+	}
+
+}
---
+++ covidafterparty/src/test/java/com/pai/covidafterparty/CovidafterpartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pai.covidafterparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidafterpartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
