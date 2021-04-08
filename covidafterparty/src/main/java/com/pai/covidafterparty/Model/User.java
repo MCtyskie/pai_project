f128f7daee2eb116ae695166ddff429a2760ef6b
MCtyskie <micsul000@utp.edu.pl>
2021-04-08 12:25:41 +0200

    Update User.java

--- covidafterparty/src/main/java/com/pai/covidafterparty/Model/User.java
+++ covidafterparty/src/main/java/com/pai/covidafterparty/Model/User.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -25,10 +27,17 @@ public class User {
     private LocalDate birthdate;
     private String city;
     private String phone;
+    private boolean activeAccount;
 
     @ManyToMany(mappedBy = "moderators")
     @JsonIgnore
     private List<Event> moderating;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name="users_to_roles",
+            joinColumns = @JoinColumn(name="userID"),
+            inverseJoinColumns = @JoinColumn(name="roleID"))
+    private Set<Role> roles=new HashSet<>();
 
     public User(String name, String lastName, String email, String password, LocalDate birthdate, String city, String phone) {
         this.name = name;
