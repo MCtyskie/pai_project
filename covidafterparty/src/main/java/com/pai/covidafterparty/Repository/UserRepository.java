a4e53a2c436eedf7c36296ba4d06d1a09da09a10
MCtyskie <micsul000@utp.edu.pl>
2021-04-08 12:25:43 +0200

    Update UserRepository.java

--- covidafterparty/src/main/java/com/pai/covidafterparty/Repository/UserRepository.java
+++ covidafterparty/src/main/java/com/pai/covidafterparty/Repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.pai.covidafterparty.Repository;
 import com.pai.covidafterparty.Model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    public Optional<User> findUserByEmail(String email);
 }
