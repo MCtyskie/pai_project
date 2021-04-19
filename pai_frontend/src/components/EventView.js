6235013953786bfa9286d4af68b9815fcf6d6c6a
plywakd <46456311+plywakd@users.noreply.github.com>
2021-04-19 21:03:09 +0200

    Update EventView.js
    Testing table generation on dummy data

--- pai_frontend/src/components/EventView.js
+++ pai_frontend/src/components/EventView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 import "./event.css"
 
 class EventView extends React.Component {
@@ -11,6 +12,7 @@ class EventView extends React.Component {
 		}
 		this.fetchAllEvents = this.fetchAllEvents.bind(this);
 		this.dummyEvents = this.dummyEvents.bind(this);
+		this.prepareTable = this.prepareTable.bind(this);
 	}
 
 	componentDidMount() {
@@ -21,16 +23,16 @@ class EventView extends React.Component {
 	fetchAllEvents() {
 		console.log("TODO fetch events");
 		this.setState({ isFetchingData: true });
-		const backend_url = "http://localhost:8081/event/events";
-		axios.get(backend_url)
-			.then(response => {
-				this.setState({ eventList: response.data, isFetchingData: false });
-			})
-			.catch(err => {
-				console.log(err);
+		// const backend_url = "http://localhost:8081/event/events";
+		// axios.get(backend_url)
+		// 	.then(response => {
+		// 		this.setState({ eventList: response.data, isFetchingData: false });
+		// 	})
+		// 	.catch(err => {
+		// 		console.log(err);
 
-				this.setState({ isFetchingData: false });
-			})
+		// 		this.setState({ isFetchingData: false });
+		// 	})
 		this.dummyEvents();
 	}
 
@@ -41,30 +43,56 @@ class EventView extends React.Component {
 					"title": "meloinferno",
 					"date": "2021-04-19 19:00:00",
 					"adress": "Bydgoszcz 85 - 435 Puławska 13",
-					"ageRestriction": true,
-					"invitationsAccepted": 23,
-					"maxGuests": 100,
+					"ageRestriction": "true",
+					"invitationsAccepted": "23",
+					"maxGuests": "100",
 					"picture": "JPG",
 				},
 				{
 					"title": "meloinferno",
 					"date": "2021-04-19 19:00:00",
 					"adress": "Bydgoszcz 85 - 435 Puławska 13",
-					"ageRestriction": true,
-					"invitationsAccepted": 23,
-					"maxGuests": 100,
+					"ageRestriction": "true",
+					"invitationsAccepted": "23",
+					"maxGuests": "100",
 					"picture": "JPG",
 				}
-			]
+			], isFetchingData: false
 		}, () => console.log(this.state.eventList));
 	}
 
-	render() {
+	prepareTable() {
 		let eventPage;
+		if (this.state.eventList.length !== 0) {
+			let headers = Object.values(this.state.eventList)[0];
+			let header = Object.keys(headers);
 
+			eventPage =
+				(<Table responsive>
+					<thead>
+						<tr>
+							{header.map((k, i) => <th key={i}>{k}</th>)}
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.state.eventList.map((r, i) => (
+								<tr key={i}>{
+									Object.values(r).map((resval, j) => <td key={j}>{resval.toString()}</td>)
+								}
+								</tr>
+							))
+						}
+					</tbody>
+				</Table>)
+		}
+		return eventPage;
+	}
+
+	render() {
 		return (
 			<div className="event-container">
-				{eventPage}
+				{this.state.isFetchingData ? "fetching data..." : this.prepareTable()}
 			</div>
 		);
 	}
