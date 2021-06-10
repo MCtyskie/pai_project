fd3256a84c153ff153a7f3ecc9cfa76ffddf9f60
plywakd <46456311+plywakd@users.noreply.github.com>
2021-06-11 01:19:28 +0200

    Update EventInvitations.js

--- pai_frontend/src/components/Invitation/EventInvitations.js
+++ pai_frontend/src/components/Invitation/EventInvitations.js
@@ -14,11 +14,39 @@ class EventInvitations extends React.Component {
         }
         this.fetchOrganisedEvents = this.fetchOrganisedEvents.bind(this);
         this.prepareEventInvitations = this.prepareEventInvitations.bind(this);
+        this.updateInvitationStatus = this.updateInvitationStatus.bind(this);
+
     }
 
     componentDidMount() {
         this.fetchOrganisedEvents();
     }
+
+    updateInvitationStatus(invitation, newStatus) {
+        const backend_url = "http://localhost:8081/api/invitation/update";
+        let data = {
+            invitationID: invitation["invitationID"],
+            inviterID: invitation["inviterID"],
+            inviterName: invitation["inviterName"],
+            invitedID: invitation["invitedID"],
+            invitedName: invitation["invitedName"],
+            eventID: invitation["eventID"],
+            status: newStatus,
+        }
+        console.log(data);
+        axios.post(backend_url, data, {
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem('token').substring(1).slice(0, -1)}`,
+            }
+        }).then(response => {
+            console.log(response.data);
+            this.setState({ isFetchingData: true }, () => this.fetchOrganisedEvents());
+        }).catch(err => {
+            console.log("something wrong happend");
+            console.log(err);
+        })
+    }
+
 
     fetchOrganisedEvents() {
         const backend_url = "http://localhost:8081/api/event/manageEvents";
@@ -29,7 +57,7 @@ class EventInvitations extends React.Component {
         })
             .then(response => {
                 // We get list of pairs so first is always an event and second is list of invitations for this events
-                this.setState({ eventList: response.data, isFetchingData: false  });
+                this.setState({ eventList: response.data, isFetchingData: false });
             })
             .catch(err => {
                 console.log(err);
@@ -39,15 +67,34 @@ class EventInvitations extends React.Component {
 
     prepareEventInvitations() {
         let eventInvitations = [];
-        this.state.eventList.forEach(pair =>{
+        this.state.eventList.forEach(pair => {
             console.log(pair["first"]);
             console.log(pair["second"]);
+            let invitationsTab = []
+            if (pair["second"].length !== 0) {
+                pair["second"].forEach(invitation => {
+                    invitationsTab.push(
+                        <div>
+                            <div>
+                                {invitation["invitedName"]} | status : {invitation["status"]}
+                            </div>
+                            <Button variant="primary" onClick={() => this.updateInvitationStatus(invitation, "ACCEPTED")}>Accept</Button>
+                            <Button variant="secondary" onClick={() => this.updateInvitationStatus(invitation, "DENIED")}>Discard</Button>
+                        </div>
+                    )
+                })
+            }
+            else {
+                invitationsTab.push(
+                    <div>
+                        No invitations yet
+                    </div>
+                )
+            }
             eventInvitations.push(
                 <div>
-                    <EventRow item={pair["first"]}/>
-                    <div>{pair["second"]["status"]} | {pair["second"]["invitedName"]}</div>
-                    <Button variant="primary">Accept</Button>
-                    <Button variant="secondary">Discard</Button>
+                    <EventRow item={pair["first"]} />
+                    {invitationsTab}
                 </div>
             )
         })
